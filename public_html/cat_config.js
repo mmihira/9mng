5313b1f6c9239f47868ecc99fcc1ffd6a1bec61a
unknown <mmihira.w@gmail.com>
2015-03-19 15:43:10 +1100

    The save categorise and recategorise data buttons now work

--- public_html/cat_config.js
+++ public_html/cat_config.js
@@ -5,7 +5,16 @@ _glbl.fns.goToCatConfig = function()
     $(glel.cnf_parent).contents().detach();
     $(glel.cnf_cat_view_main).contents().detach();
     
-    // Remove the only references to previously created panels.
+    /******************* Cat Editing Panel Creation *******************
+     *
+     * The structure of glel.panelvec is like :
+     * [{   main : div element which holds the title and panel
+     *      title_div : div element which holds the title_div_table_cell
+     *      title_div_table_cell : div element for holding the element whith the title itself
+     *      cat_terms : the textarea element which hols the category term. 
+     *      },...]
+     * each {} in the vector is an panel.
+     */
     glel.panelvec = [];
     _glbl.catg.updateCatlist();
     var pRef = false;
@@ -38,8 +47,7 @@ _glbl.fns.goToCatConfig = function()
             {
                 pRef.cat_terms.innerHTML += _glbl.catlist[i][j] + "\n"; 
             }
-            
-            
+   
         // Append to the main panel div
         pRef.main.appendChild(pRef.title_div);
         pRef.main.appendChild(pRef.cat_terms);
@@ -51,10 +59,49 @@ _glbl.fns.goToCatConfig = function()
         glel.cnf_cat_view_main.appendChild(glel.panelvec[i].main);
     }  
     
-    // Create the category table view
+    /******************* Cat Display Table *******************/
+    _glbl.catg.rfrsgCatTable();
+    
+    
+    
+    
     glel.cnf_parent.appendChild(glel.cnf_choice_main);
     glel.cnf_parent.appendChild(glel.cnf_cat_view_main);
+    glel.cnf_parent.appendChild(glel.cnf_cat_table_main);
     _glbl.main.append(glel.cnf_parent);
+};
+
+/*
+ * The refersh cat table function is called to referesh the category table.
+ * Called during page start and also when data is recategorised.
+ */
+
+_glbl.catg.rfrsgCatTable = function()
+{
+    // Remove the references to any existing tables
+    $(glel.cnf_cat_table).contents().detach();
+    glel.ctvec = [];
+    
+    var cols_to_inc = [[_glbl.dbs.acc],[_glbl.dbs.desc],[_glbl.dbs.cat]];
+    
+    for(var i in _glbl.db.all_data)
+    {
+        glel.ctvec.push({});
+        var tRef = glel.ctvec[glel.ctvec.length -1];
+        
+        tRef.row = document.createElement("div");
+        tRef.row.className = "cattablerow";
+        for(var j in cols_to_inc)
+        {
+            tRef.cellv = [];
+            var k = document.createElement("div");
+            k.className = "cattablecell";
+            k.innerHTML = _glbl.db.all_data[i][cols_to_inc[j]];
+            tRef.cellv.push({x : k});
+            tRef.row.appendChild(k);
+        }
+        glel.cnf_cat_table.appendChild(tRef.row);
+    }
 };
 
 _glbl.fns.createCatConfigPage =  function()
@@ -78,7 +125,14 @@ _glbl.fns.createCatConfigPage =  function()
             glel.cnf_choice_btn_save= document.createElement("div");
             glel.cnf_choice_btn_save.className = "cnfchoicebtn";
             glel.cnf_choice_btn_save.innerHTML = "Save Categories";
-            $(glel.cnf_choice_btn_save).on("click",function(){});
+            $(glel.cnf_choice_btn_save).on("click",_glbl.catg.savecat);
+            glel.cnf_choice_spacer2 = document.createElement("div");
+            glel.cnf_choice_spacer2.className = "cnfbtnspacer";
+            // The recategorise button
+            glel.cnf_choice_btn_recat = document.createElement("div");
+            glel.cnf_choice_btn_recat.className = "cnfchoicebtn";
+            glel.cnf_choice_btn_recat.innerHTML = "Recategorise Data";
+            $(glel.cnf_choice_btn_recat).on("click",_glbl.catg.config_categorise);
             // The add category button
             // The recat button
             
@@ -86,11 +140,18 @@ _glbl.fns.createCatConfigPage =  function()
         glel.cnf_choice_main.appendChild(glel.cnf_choice_btn_dashboard);
         glel.cnf_choice_main.appendChild(glel.cnf_choice_spacer1);
         glel.cnf_choice_main.appendChild(glel.cnf_choice_btn_save);
+        glel.cnf_choice_main.appendChild(glel.cnf_choice_spacer2);
+        glel.cnf_choice_main.appendChild(glel.cnf_choice_btn_recat);
             
         // Div to hold the category editing panel
         glel.cnf_cat_view_main = document.createElement("div");
         glel.cnf_cat_view_main.className = "cnfcatviewmain";
         
-        // Variable to hole the editing panels
-        glel.panelvec = [];        
+        // Div to hold the table of data showing category
+        glel.cnf_cat_table_main = document.createElement("div");
+        glel.cnf_cat_table_main.className = "cnfcattablemain";
+            glel.cnf_cat_table = document.createElement("div");
+            glel.cnf_cat_table.className = "cnfcattable";
+        glel.cnf_cat_table_main.appendChild(glel.cnf_cat_table);  
+                
 };
--- public_html/categorisefns.js
+++ public_html/categorisefns.js
@@ -29,6 +29,52 @@ _glbl.cat = {
 // A vector used to hold a list of the categories in _glbl.cat
 _glbl.catlist = {};
 
+
+/*
+ * Called by the reccategorise button in the category config
+ * page to recategorise the data. Also the category table is refreshed.
+ * 
+ */
+_glbl.catg.config_categorise = function()
+{
+    var x = _glbl.db.all_data;            // Temporary reference to the all_data database. 
+    // Loop through all the data in all_data and categorise as necesary.
+    for( var i in x)
+    {
+        x[i][_glbl.dbs.cat] = _glbl.catg.categorise(x[i],_glbl.dbs.desc); 
+    }
+    
+    // Refresh the category table.
+    _glbl.catg.rfrsgCatTable();
+    
+};
+
+/**
+ * Called by the save category button to save the category setups in the pannel
+ */
+_glbl.catg.savecat = function()
+{
+    var r = window.confirm(" Are your sure you want to save the category data ?\nNote : Cat data will not be saved on your hard disk.\nClick update DB on the dasboard to do so after saving cat data.");
+    if(r === true)
+    {
+        _glbl.cat = {};
+        for( var i in glel.panelvec)
+        {
+            // create a vector of the cat terms
+            var catrmsv = glel.panelvec[i].cat_terms.value.split("\n");
+            for(var k in catrmsv)
+            {
+                // Ignore empty values;
+                if(catrmsv[k].length > 0)
+                {
+                    _glbl.catg.addCat(catrmsv[k],glel.panelvec[i].title_div_table_cell.innerHTML);
+
+                }
+            }
+        }
+    }   
+};
+
 /**
 * Called by cnPapaComplete and reCat to Categorise the data
 * 
