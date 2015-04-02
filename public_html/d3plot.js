b4c3ad0b6ed68b8b9f795a8573ed5f6c58cf4105
unknown <mmihira.w@gmail.com>
2015-04-02 12:25:04 +1100

    Converted all charts to D3
    - The plot_charts.js is deprecated and kept only as an archive
    - d3plot.js now created the newcashflow as well as the netposition charts

--- public_html/d3plot.js
+++ public_html/d3plot.js
@@ -10,6 +10,7 @@ _glbl.d3plot.createNetCashFlow = function(yrs)
                             yrs:yrs ,
                             mo:["all"]                          
                         });
+                        
     var gi = _glbl.dbint.get_data({  acc:"GOAL",
                             val_bal:"val",
                             deb_cred:"cred",
@@ -20,6 +21,7 @@ _glbl.d3plot.createNetCashFlow = function(yrs)
                             yrs:yrs ,
                             mo:["all"]                          
                         });
+                        
     var so = _glbl.dbint.get_data({  acc:"SAVER",
                             val_bal:"val",
                             deb_cred:"deb",
@@ -63,10 +65,6 @@ _glbl.d3plot.createNetCashFlow = function(yrs)
             }
         }
     }
-    
-    //var _data = [{name:"A",value:.08167},{name:"B",value:.01492},{name:"C",value:.02782},{name:"D",value:.04253},{name:"E",value:.12702},{name:"F",value:.02288},{name:"G",value:.02015},{name:"H",value:.06094},{name:"I",value:.06966},{name:"J",value:.00153},{name:"K",value:.00772},{name:"L",value:.04025},{name:"M",value:.02406},{name:"N",value:.06749},{name:"O",value:.07507},{name:"P",value:.01929},{name:"Q",value:.00095},{name:"R",value:.05987},{name:"S",value:.06327},{name:"T",value:.09056},{name:"U",value:.02758},{name:"V",value:.00978},{name:"W",value:.02360},{name:"X",value:.00150},{name:"Y",value:.01974},{name:"Z",value:.00074}];
-    
-    console.log(_data);
     
     var margin = {top: 20, right: 30, bottom: 30, left: 80},
     width = 1200 - margin.left - margin.right,
@@ -171,4 +169,159 @@ _glbl.d3plot.createNetCashFlow = function(yrs)
     
       
       
+};
+
+_glbl.d3plot.createNewNetPosition = function(yrs)
+{
+    var _data   = [];
+
+    var svrd    = [];
+    var goald   = [];
+    var netd    = [];
+
+    var svrd    = _glbl.dbint.get_net_position(yrs,"SAVER");
+    var netd    = _glbl.dbint.get_net_position(yrs,"NETBANK");
+    var goald   = _glbl.dbint.get_net_position(yrs,"GOAL");
+
+    // Determine which off the accounts has the most data.
+    var srt = [[svrd,function(l,r){return parseFloat(l) + parseFloat(r);}], 
+               [netd,function(l,r){return parseFloat(l) + parseFloat(r);}], 
+               [goald,function(l,r){return parseFloat(l) + parseFloat(r);}]
+                ];
+    srt.sort(function(x,y){return x[0].length-y[0].length;});
+    
+    // Make the data vector of format [[yr-mo as string,float value],.....]    
+    // We make the date vector big enough to fit the largest data length.
+    for(var i in srt[srt.length-2][0])
+    {
+        _data.push({name:srt[srt.length-1][0][i][0],value:0.0,next:{},not_last:false});
+    }
+
+    for(var i = 0 ; i < srt.length; i++)
+    {
+        var accvref = srt[i][0];
+        for(var k = 0; k < accvref.length; k++)
+        {
+            //make sure we are looking at the same month
+            if( accvref[k][0] === _data[k]["name"])
+            {
+                // format is like this 
+                //[{name:$$ ,vlaue:$$ },{}]
+                _data[k]["value"] = srt[i][1](_data[k]["value"],accvref[k][1]);
+            }
+        }
+    }
+    
+    // Fill in the next element
+    // The last element will have a not_last element being true;
+    for(var i = 0; i < _data.length-1 ; i++)
+    {
+        _data[i]["next"] = _data[i+1];
+    }
+    _data[_data.length-1]["next"]["not_last"] = true;
+    
+    var margin = {top: 20, right: 30, bottom: 30, left: 80},
+    width = 1200 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+        
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+        //.ticks(10, "%");
+    
+    /* These methods don't work :
+    var svg = d3.select("#stest")
+    var svg = d3.select("#dsb_d3netcashflow_svg")
+    hardcoding the svg in works, but creating the svg via document.createElement(svg) doesn't
+    might have to call document.createElementNS but haven't tested it.
+    // I can only seem to get it to work like this */
+    var svg = d3.select("#dsb_d3netposition").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("float","left")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    /*d3.tsv("data.tsv", type, function(error, data) {
+      x.domain(data.map(function(d) { return d.name; }));
+      y.domain([0, d3.max(data, function(d) { return d.value; })]);*/
+    x.domain(_data.map(function(d) { return d.name; }));
+    y.domain([d3.min(_data, function(d) { return d.value; }), d3.max(_data, function(d) { return d.value; })]);
+    
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("$AUSD");
+    
+    var d3line = svg.selectAll(".b").data(_data);
+    d3line.enter().append("line")
+      .attr("class", "netposline")
+      .attr("x1", function(d) { return (x(d.name) + x.rangeBand()/2) ; })
+      .attr("x2", function(d) {
+          if(!d.next.not_last)
+            { 
+                return (x(d.next.name) + x.rangeBand()/2) ; 
+            }else{
+                return (x(d.name) + x.rangeBand()/2); 
+            }
+        })
+      .attr("y1", function(d) {return y(d.value);})
+      .attr("y2", function(d) {
+          if(!d.next.not_last)
+            { 
+                return y(d.next.value); 
+            }else{
+                return y(d.value); 
+            }
+        });
+               
+      
+    /*d3bars.enter().append("text")
+        .attr("class", "bartext")
+      .attr("x", function(d){return x(d.name) +x.rangeBand()/2;})
+      .attr("y", function(d){
+          if(d.value >= 0){
+                return y(d.value)+3;
+            }
+            else{
+                return y(d.value)-3;
+            }
+        })
+      .attr("dy", function(d) {
+          if(d.value >= 0){
+                return "0.75em";
+            }
+            else{
+                return "-0.75em";
+            }
+        })
+      .attr("dx",function(d){
+            return "-"+(String(Math.round(d.value)).length/2) + "em";
+        })
+      .text(function(d) { return Math.round(d.value); });*/
+    
+    
+    
+    
 };
--- public_html/dashboard.js
+++ public_html/dashboard.js
@@ -5,10 +5,14 @@ _glbl.fns.goToDashboard = function()
     _glbl.main.contents().detach();    
     _glbl.main.append(glel.dsb_main);
     
+    // Remove existing netcashflow svg
+    $("#dsb_d3netcashflow").contents().detach();
+    $("#dsb_d3netposition").contents().detach();
     
-    _glbl.flotplot.createNetCashFlow(["2014","2015"]);
-    _glbl.flotplot.createNewNetPosition(["2014","2015"]);
+    // Create the charts
     _glbl.d3plot.createNetCashFlow(["2014","2015"]);
+    _glbl.d3plot.createNewNetPosition(["2014","2015"]);
+    
     
 };
 
@@ -55,37 +59,32 @@ _glbl.fns.createDashboard = function()
         glel.dsb_netcashflow_title.className = "netCashFlowTitle";
         glel.dsb_netcashflow_title.innerHTML = "<br>Net Cash Flow<br>";
     
-        // The div to hold the net cash flow chart
-        glel.dsb_netcashflow_flot = document.createElement("div");
-        glel.dsb_netcashflow_flot.id ="dsb_netcashflow_flot";
-        glel.dsb_netcashflow_flot.className = "dsbnetcashflowflot";        
-        glel.dsb_netcashflow_flot.style.width = "1200px";        
-        glel.dsb_netcashflow_flot.style.height = "300px";
+        // Div to hold the d3 net cash flow chart
+        glel.dsb_d3netcashflow = document.createElement("div");
+        glel.dsb_d3netcashflow.id = "dsb_d3netcashflow";
+        glel.dsb_d3netcashflow.className = "dsbd3netcashflow";
+        glel.dsb_d3netcashflow.width = "1200px";
+        //glel.dsb_d3netcashflow.height = "300px"; 
         
         // Div to to hold the title name
         glel.dsb_netposition_title = document.createElement("div");
         glel.dsb_netposition_title.className = "netPositionTitle";
         glel.dsb_netposition_title.innerHTML = "<br>Net Position<br>";        
         
-        // The div to hold the net position.
-        glel.dsb_netposition_flot = document.createElement("div");
-        glel.dsb_netposition_flot.id ="dsb_netPosition_flot";
-        glel.dsb_netposition_flot.className = "dsbnetPositionFlot";        
-        glel.dsb_netposition_flot.style.width = "1200px";        
-        glel.dsb_netposition_flot.style.height = "300px";
+        // Div to hold the d3 net position chart
+        glel.dsb_d3netposition = document.createElement("div");
+        glel.dsb_d3netposition.id = "dsb_d3netposition";
+        glel.dsb_d3netposition.className = "dsbd3netposition";
+        glel.dsb_d3netposition.width = "1200px";
+        //glel.dsb_d3netcashflow.height = "300px";
         
-        // Test div to hold the d3 chart
-        glel.dsb_d3netcashflow = document.createElement("div");
-        glel.dsb_d3netcashflow.id = "dsb_d3netcashflow";
-        glel.dsb_d3netcashflow.className = "dsbd3netcashflow";
-        //glel.dsb_d3netcashflow.width = "1200px";
-        //glel.dsb_d3netcashflow.height = "300px";         
+                
     
     glel.dsb_main.appendChild(glel.dsb_choice_main);
     glel.dsb_main.appendChild(glel.dsb_netcashflow_title);
-    glel.dsb_main.appendChild(glel.dsb_netcashflow_flot);
-    glel.dsb_main.appendChild(glel.dsb_netposition_title);
-    glel.dsb_main.appendChild(glel.dsb_netposition_flot);
     glel.dsb_main.appendChild(glel.dsb_d3netcashflow);
+    glel.dsb_main.appendChild(glel.dsb_netposition_title);
+    glel.dsb_main.appendChild(glel.dsb_d3netposition);
+    
     
 };
