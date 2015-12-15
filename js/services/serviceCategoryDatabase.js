a4a7ba03c64a7bf56eca96add4056f4dd2db540e
mmihira <mmihira.w@gmail.com>
2015-12-15 18:03:42 +1100

    Commenting

--- js/services/serviceCategoryDatabase.js
+++ js/services/serviceCategoryDatabase.js
@@ -15,7 +15,7 @@ angular.module('service.categoryDatabase',['service.categoryClass']).service('ca
      * Holds categoryClass objects which reperesent unique categories
      * used to identify transactions. catDb.dB is a map where the key
      * is the name of the category and the value is the categoryClass
-     * object.
+     * object. Ex {food: Object,...}
      */
     catDB.dB = {};
 
@@ -23,7 +23,7 @@ angular.module('service.categoryDatabase',['service.categoryClass']).service('ca
      * dBIdentifiers is map where the keys are unqiue strings and the value 
      * is the associated categoryClass object. That is the presense of the
      * string in a description indicates the transaction belongs to the
-     * associated category.
+     * associated category. { 'identyfying string': Object }
      */
     catDB.dBIdentifiers = {};
 
